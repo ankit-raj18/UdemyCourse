1a062c349d75889f058d15b4c34a964684b89fd7
Ankitraj2000 <kumarankitraj28@gmail.com>
2021-04-30 03:25:18 +0530

    New updates as on 30.04

--- App.js
+++ App.js
@@ -6,9 +6,12 @@ import ComponentsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen'
 import ImageScreen from "./src/screens/ImageScreen";
 import CounterScreen from './src/screens/CounterScreen';
+import CounterScreenReducer from './src/screens/CounterScreenReducer';
 import ColorScreen from "./src/screens/ColorScreen";
 import SquareScreen from "./src/screens/SquareScreen";
 import SquareScreenReducer from "./src/screens/SquareScreenReducer";
+import TextScreen from './src/screens/TextScreen';
+import BoxScreen from './src/screens/BoxScreen';
 
 const navigator = createStackNavigator(
   {
@@ -17,9 +20,12 @@ const navigator = createStackNavigator(
     List : ListScreen,
     Image: ImageScreen,
     Counter : CounterScreen,
+    CounterReducer : CounterScreenReducer,
     Color: ColorScreen,
     Square: SquareScreen,
     SquareReducer : SquareScreenReducer,
+    Text :TextScreen,
+    Box : BoxScreen,
   },
   {
     initialRouteName: "Home",
